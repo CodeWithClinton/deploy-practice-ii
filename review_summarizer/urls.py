9610343cf5a76e955ec2d1b1f5087ea248b22997
CodeWithClinton <clintonmatics@gmail.com>
2026-01-19 17:59:05 +0100

    added deploy readme

--- review_summarizer/urls.py
+++ review_summarizer/urls.py
@@ -25,38 +25,3 @@ urlpatterns = [
 
 
 
-
-# [Unit]
-# Description=gunicorn daemon
-# Requires=gunicorn.socket
-# After=network.target
-
-# [Service]
-# User=chibunna
-# Group=www-data
-# WorkingDirectory=/home/chibunna/deploy-pratice-i
-# ExecStart=/home/chibunna/deploy-pratice-i/venv/bin/gunicorn \
-#           --access-logfile - \
-#           --workers 3 \
-#           --bind unix:/run/gunicorn.sock \
-#           review_summarizer.wsgi:application
-
-# [Install]
-# WantedBy=multi-user.target
-
-
-
-# server {
-#     listen 80;
-#     server_name 138.68.158.95;
-
-#     location = /favicon.ico { access_log off; log_not_found off; }
-#     location /staticfiles/ {
-#         root /home/chibunna/deploy-practice-i;
-#     }
-
-#     location / {
-#         include proxy_params;
-#         proxy_pass http://unix:/run/gunicorn.sock;
-#     }
-# }
